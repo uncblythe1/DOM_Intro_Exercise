d8000a259ddf8f8c230d4c54741a00bbf1c7c167
Blythe Totsch <uncblythe1@gmail.com>
2018-08-08 08:44:13 -0500

    completed checkerboard(red/black tiles) section

--- app.js
+++ app.js
@@ -5,25 +5,32 @@ document.addEventListener('DOMContentLoaded', function () {
     const greet = document.getElementById('greeting')
     greet.innerText = 'Hello, World!'
 
-    const li = document.getElementsByTagName('li')
-        for (let i of li) {
-            i.style.backgroundColor = 'yellow'
+    const drinks = document.getElementsByTagName('li')
+    //loop through the drinks
+        //make drinks background-color yellow    
+    for (let drink of drinks) {
+            drink.style.backgroundColor = 'yellow'
         }
 
-    let newImage = document.createElement('img')
+    const newImage = document.createElement('img')
     newImage.setAttribute('src', `http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif`);
     greet.appendChild(newImage)
 
-    let newList = document.createElement('ul')
+    const newList = document.createElement('ul')
     newList.className = 'todo-items'
     document.body.appendChild(newList)
    
     
     const chores = ['make coffee', 'eat donut', 'change diapers', 'drive to work']
-        chores.forEach(function(item) {
+        chores.forEach(function(chore) {
             let li = document.createElement('li')//create new li element
-            let text = document.createTextNode(item);//create a text node w/text from array
+            let text = document.createTextNode(chore);//create a text node w/text from array
             li.appendChild(text);//add text to li element
             newList.appendChild(li);//add li element to ul "todo-items"
+        //for (const chore of chores) {
+        // 
+        //    
+        //}
+        
         })
 })
